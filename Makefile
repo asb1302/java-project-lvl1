332d1453618455f44fc815db09f2100d78ae9625
Alexey B <you@example.com>
2022-01-22 21:43:23 +0300

    finish step 3

---
+++ Makefile
@@ -0,0 +1,27 @@
+.DEFAULT_GOAL := build-run
+
+setup:
+	gradle wrapper --gradle-version 7.3.3
+
+clean:
+	./gradlew clean
+
+build:
+	./gradlew clean build
+
+install:
+	./gradlew clean install
+
+run-dist:
+	./app/build/install/app/bin/app
+
+check-updates:
+	./gradlew dependencyUpdates
+
+update-deps:
+	./gradlew useLatestVersions
+
+build-run: build run
+
+run:
+	./gradlew run
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+
+        String name = input.nextLine();
+
+        // prints the name
+        System.out.println("Hello, " + name + "!");
+
+        // closes the scanner
+        input.close();
     }
 }
