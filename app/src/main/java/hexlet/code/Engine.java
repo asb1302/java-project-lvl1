e3fa336b249d61cb82c03cf6518afe193503ae28
Alexey B <you@example.com>
2022-05-24 09:10:25 +0300

    finish step 9

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -6,6 +6,7 @@ import hexlet.code.games.GameInterface;
 import hexlet.code.games.EvenGame;
 import hexlet.code.games.GCDGame;
 import hexlet.code.games.CalcGame;
+import hexlet.code.games.PrimeGame;
 
 import java.util.Scanner;
 
@@ -15,6 +16,7 @@ public class Engine {
     private final GameInterface evenGame;
     private final GameInterface gcdGame;
     private final GameInterface progressionGame;
+    private final GameInterface primeGame;
 
     public static final int GREET_GAME_NUM = 1;
     public static final int EVEN_GAME_NUM = 2;
@@ -22,6 +24,7 @@ public class Engine {
 
     public static final int GCD_GAME_NUM = 4;
     public static final int PROGRESSION_GAME_NUM = 5;
+    public static final int PRIME_GAME_NUM = 6;
     public static final int EXIT_NUM = 0;
 
     public Engine() {
@@ -30,6 +33,7 @@ public class Engine {
         this.evenGame = new EvenGame();
         this.gcdGame = new GCDGame();
         this.progressionGame = new ProgressionGame();
+        this.primeGame = new PrimeGame();
     }
 
     public final void execute() {
@@ -39,6 +43,7 @@ public class Engine {
         System.out.println(CALC_GAME_NUM + " - Calc");
         System.out.println(GCD_GAME_NUM + " - GDC");
         System.out.println(PROGRESSION_GAME_NUM + " - Progression");
+        System.out.println(PRIME_GAME_NUM + " - Prime");
         System.out.println(EXIT_NUM + " - Exit");
         System.out.print("Your choice: ");
 
@@ -64,6 +69,9 @@ public class Engine {
                 }
                 case PROGRESSION_GAME_NUM -> {
                     this.progressionGame.play();
+                }
+                case PRIME_GAME_NUM -> {
+                    this.primeGame.play();
                 }
                 case EXIT_NUM -> {
                     System.out.println("Bye!");
